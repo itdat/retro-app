c0928de3a09bf2b33a175382ec90c4d5ab2ebd37
Nguyen Tuan Dat <53074785+itdat@users.noreply.github.com>
2020-10-28 01:07:43 +0700

    Validate User model

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const connectDB = require("./config/db");
 // Connect DB
 connectDB();
 
+// Init middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => {
   res.json({ msg: "Welcome to the Retrospective API..." });
 });
