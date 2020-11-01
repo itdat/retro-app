3c73c03c569bacc2080ee85ac68b83e9e20bf2ad
Nguyen Tuan Dat <53074785+itdat@users.noreply.github.com>
2020-11-01 12:48:29 +0700

    Refactor Navbar component

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -2,13 +2,25 @@ import React from "react";
 import Appbar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import CameraIcon from "@material-ui/icons/PhotoCamera";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 import { makeStyles } from "@material-ui/core/styles";
+import { Avatar, Button, Grid } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  icon: {
+  flex: {
+    flex: 1,
+  },
+  widthAuto: {
+    width: "auto",
+  },
+  iconBtn: {
     marginRight: theme.spacing(2),
   },
+  username: {
+    marginRight: theme.spacing(1),
+  },
+  toolbarMargin: theme.mixins.toolbar,
 }));
 
 const Navbar = () => {
@@ -16,10 +28,26 @@ const Navbar = () => {
   return (
     <Appbar position="relative">
       <Toolbar>
-        <CameraIcon className={classes.icon} />
-        <Typography variant="h6" color="inherit">
+        <IconButton
+          className={classes.iconBtn}
+          color="inherit"
+          aria-label="Menu"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" color="inherit" className={classes.flex}>
           Retrospective App
         </Typography>
+        {/* <Button color="inherit">Login</Button> */}
+
+        <Grid container alignItems="center" className={classes.widthAuto}>
+          <Grid item>
+            <Typography className={classes.username}>Username</Typography>
+          </Grid>
+          <Grid item>
+            <Avatar>TD</Avatar>
+          </Grid>
+        </Grid>
       </Toolbar>
     </Appbar>
   );
