9e1a07db19dd46f2e050347a3c066e87efd69938
Nguyen Tuan Dat <53074785+itdat@users.noreply.github.com>
2020-11-03 14:06:39 +0700

    Add feature to edit card content

--- client/src/components/cards/RetroCard.jsx
+++ client/src/components/cards/RetroCard.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import { Hidden } from "@material-ui/core";
+import { Hidden, TextField } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -18,13 +18,28 @@ const useStyles = makeStyles((theme) => ({
 
 const RetroCard = ({ card }) => {
   const classes = useStyles();
+
+  const [edit, setEdit] = useState(false);
+  const [value, setValue] = useState(card.content);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleModifyClick = () => {
+    setEdit(!edit);
+  };
+
   return (
     <Card className={classes.card}>
       <CardContent className={classes.cardContent}>
-        <Typography gutterBottom variant="h5" component="h2">
-          {card.content}
-        </Typography>
-        <Typography>{card.content}</Typography>
+        {edit ? (
+          <TextField multiline value={value} onChange={handleChange} />
+        ) : (
+          <Typography multiline style={{ whiteSpace: "pre-line" }}>
+            {value}
+          </Typography>
+        )}
       </CardContent>
       <CardActions>
         <Hidden mdUp>
@@ -32,8 +47,8 @@ const RetroCard = ({ card }) => {
             Move
           </Button>
         </Hidden>
-        <Button size="small" color="primary">
-          Edit
+        <Button size="small" color="primary" onClick={handleModifyClick}>
+          {edit ? "Save" : "Edit"}
         </Button>
       </CardActions>
     </Card>
